2a61c193c85e5f2fba8f86db8bf83197ec3a22dc
Peter Rush <peter.rush@enactor.co.uk>
2021-11-18 08:32:39 +0000

    Initial Commit

---
+++ SwiftUI SelfScan/SwiftUI_SelfScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SelfScanApp.swift
+//  SwiftUI SelfScan
+//
+//  Created by Peter Rush on 18/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SelfScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
